940d763b6459d492e2fe161babb876be17b334bd
hamzajamil27 <90316652+hamzajamil27@users.noreply.github.com>
2022-11-17 00:11:48 +0500

    Basic Lottery App
    This is a Basic Lottery app

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,83 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:hello/page1.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp( const MyApp());
+}
+class MyApp extends StatefulWidget {
+   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  
-  // This widget is the root of your application.
+Random random = Random();
+int x= 0;
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-    home:  Scaffold(
-    body: page1(),
-    backgroundColor: Colors.yellow[100],
-     ),
-     
-    );
+    return  MaterialApp(
+     home: Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text("My Lottery App", 
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+        ),
+      ), 
+      
+      body: SafeArea(
+        child: Column(
+          children: [
+
+            const SizedBox(height: 100,),
+
+            const Text("Lottery winning Number is 6"),
+      
+       const SizedBox(height: 30,),
+
+            Center(
+              child: Container(
+                
+                height: 200, 
+                width: 200,
+                decoration: const BoxDecoration(
+                color: Colors.grey,
+                ),
+
+                child: Center(
+                  child: x==6 ? const Text("Condition is matched \n you have won the lottery ",
+                                 textAlign: TextAlign.center,):
+                  
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Icon(Icons.error),
+                       Text("Your number is $x so try your luck again ", 
+                       textAlign: TextAlign.center ,),
+
+                    ],
+                 ),
+                ),
+              ),
+            ),
+          ],
+         ),
+        ),
+
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        x = random.nextInt(10);
+     // print(x);  
+        setState(() {
+          
+        });
+      },
+        child: const Icon(Icons.refresh),
+      ),
+      ),
+      );
+    
   }
 }
